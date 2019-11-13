f602bbc61835fa41589f04fae30ff1479bba0ef5
olga_urazlova <olga_urazlova@epam.com>
2019-11-13 16:27:01 +0300

    Test to train parameterize and xfail

---
+++ Lesson_2/test_parameters1.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import time
+import math
+import pytest
+
+@pytest.fixture(scope = "function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    # этот код выполнится после завершения теста
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.mark.parametrize('lesson', ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
+def test_answer(browser, lesson):
+    link = f"https://stepik.org/lesson/{lesson}/step/1"
+    browser.get(link)
+    browser.implicitly_wait(5)
+
+    answer = math.log(int(time.time()))
+    answer_input = browser.find_element_by_tag_name("textarea")
+    answer_input.send_keys(str(answer))
+    browser.find_element_by_tag_name("button").click()
+
+    correct_answer = browser.find_element_by_css_selector(".smart-hints__hint")
+    correct_answer_text = correct_answer.text
+
+    assert correct_answer_text == "Correct!", f"The hint is {correct_answer_text}"
+
+
+
+
+
