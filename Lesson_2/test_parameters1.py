4238ba6d9fe27eff82bbd1c2a6be3ba6cb86645b
olga_urazlova <olga_urazlova@epam.com>
2019-11-16 20:08:26 +0300

    Conftest contains FF and chrome

--- Lesson_2/test_parameters1.py
+++ Lesson_2/test_parameters1.py
@@ -3,15 +3,6 @@ import time
 import math
 import pytest
 
-@pytest.fixture(scope = "function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    # этот код выполнится после завершения теста
-    print("\nquit browser..")
-    browser.quit()
-
 @pytest.mark.parametrize('lesson', ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
 def test_answer(browser, lesson):
     link = f"https://stepik.org/lesson/{lesson}/step/1"
